e9e68a215905d739435a406fd27f87dc849f3abd
Sirapob Singcharoen <nicesirapob@gmail.com>
2024-06-13 15:42:07 +0700

    fix: lint

--- tests/utils/project.py
+++ tests/utils/project.py
@@ -1,3 +1,5 @@
+"""Functions for creating a cookiecut project to be used in tests."""
+
 import json
 import subprocess
 from copy import deepcopy
@@ -15,7 +17,7 @@ def initialize_git_repo(repo_dir: Path):
     subprocess.run(["git", "branch", "-M", "main"], cwd=repo_dir, check=True)
     subprocess.run(["git", "add", "--all"], cwd=repo_dir, check=True)
     subprocess.run(["git", "commit", "-m", "'feat: initial commit by pytest'"], cwd=repo_dir, check=True)
-    
+
 
 def generate_project(template_values: Dict[str, str], test_session_id: str):
     """
@@ -27,7 +29,7 @@ def generate_project(template_values: Dict[str, str], test_session_id: str):
     """
     template_values: Dict[str, str] = deepcopy(template_values)
     cookiecutter_config = {"default_context": template_values}
-    cookiecutter_config_fpath = PROJECT_DIR / f"tests/cookiecutter.json-{test_session_id}"
+    cookiecutter_config_fpath = PROJECT_DIR / f"tests/cookiecutter-{test_session_id}.json"
     cookiecutter_config_fpath.write_text(json.dumps(cookiecutter_config))
 
     cmd = [
